780d7b6af274a9c9140f5f8f3fb1cd3fafeeda4c
SeamMiner <niatit130@gmail.com>
2021-06-19 09:34:38 +0300

    Configure app

---
+++ frontend/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    pwa: {
+      themeColor: "#ff5a5a",
+      msTileColor: "#ff5a5a",
+      appleMobileWebAppCapable: "yes",
+      appleMobileWebAppStatusBarStyle: "black-translucent",
+  
+      manifestOptions: {
+        name: "Мое давление",
+        short_name: "Давление",
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#ff5a5a",
+      },  
+    }
+  }
