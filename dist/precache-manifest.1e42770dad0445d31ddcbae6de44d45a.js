7d164c2512597abb2c472f9e06cde20cf0b7911f
Dmitry Puzyrev <hello@dmitry.engineer>
2021-06-20 02:38:37 +0300

    dist

---
+++ dist/precache-manifest.1e42770dad0445d31ddcbae6de44d45a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e6e1faeed95d72be3f",
+    "url": "/css/app.daee0a5b.css"
+  },
+  {
+    "revision": "0e00179685ec3e689c37",
+    "url": "/css/auth.98c9a19d.css"
+  },
+  {
+    "revision": "6e19c2197f62e9827eb1",
+    "url": "/css/doctor.4383ad19.css"
+  },
+  {
+    "revision": "920dbbfe35af5f6850f5",
+    "url": "/css/patient.c5fb495d.css"
+  },
+  {
+    "revision": "259ec70f2cf76b25dfc268e8a2560de4",
+    "url": "/img/Blue.259ec70f.png"
+  },
+  {
+    "revision": "b51519f169353ef51fb586087c9cfe54",
+    "url": "/img/Camera.b51519f1.svg"
+  },
+  {
+    "revision": "00f03e6b24f51854a14ab2444bfd460a",
+    "url": "/img/Chevron.00f03e6b.svg"
+  },
+  {
+    "revision": "e711cae07b41cfb07cbce236eb24beb7",
+    "url": "/img/GosUslugi.e711cae0.svg"
+  },
+  {
+    "revision": "327536ff6abb961f0194155877243e65",
+    "url": "/img/Heart.327536ff.svg"
+  },
+  {
+    "revision": "f847c847dc3daaaf6939516dbb0aeb0a",
+    "url": "/img/Logo.f847c847.svg"
+  },
+  {
+    "revision": "ccdd5ecd49f754594f5628cd6e1bb875",
+    "url": "/img/Micro.ccdd5ecd.svg"
+  },
+  {
+    "revision": "c8ed356dc426b5b33efbfcf6f65d5f57",
+    "url": "/img/Tonometr.c8ed356d.png"
+  },
+  {
+    "revision": "56640547797563e2492789fb30559f50",
+    "url": "/img/Yellow.56640547.png"
+  },
+  {
+    "revision": "20b91b45f656e0090392930757950f56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91e6e1faeed95d72be3f",
+    "url": "/js/app.81df24ad.js"
+  },
+  {
+    "revision": "0e00179685ec3e689c37",
+    "url": "/js/auth.021012b7.js"
+  },
+  {
+    "revision": "3daa31342407a91b7538",
+    "url": "/js/auth~doctor~patient.faae81b5.js"
+  },
+  {
+    "revision": "acb42863dca10d6d9a53",
+    "url": "/js/chunk-vendors.4ca14c17.js"
+  },
+  {
+    "revision": "6e19c2197f62e9827eb1",
+    "url": "/js/doctor.82f172d8.js"
+  },
+  {
+    "revision": "69cdf27718dc3d7950c0",
+    "url": "/js/doctor~patient.e11ce08a.js"
+  },
+  {
+    "revision": "920dbbfe35af5f6850f5",
+    "url": "/js/patient.af621d4d.js"
+  },
+  {
+    "revision": "31602e13fad75f2340a48fa8162588d4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
