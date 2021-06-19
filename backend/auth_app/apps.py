f6a8e1f42d310f9374f057f6dae63995c0caa092
Dmitry Puzyrev <hello@dmitry.engineer>
2021-06-20 02:37:55 +0300

    dist

--- backend/auth_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthAppConfig(AppConfig):
-    name = 'auth_app'
--- backend/auth_app/views.py
+++
@@ -1,6 +0,0 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import CustomTokenObtainSerializer
-
-
-class CustomTokenObtainView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainSerializer
